806c1ea5aea36f7fa236b6648c1726f355446fca
Amine Daouma <amine.daouma.engie@gmail.com>
2024-09-22 19:06:56 +0200

    initial commit scrape https://quotes.toscrape.com/js/

--- quotes_js_scraper/spiders/quotes.py
+++ quotes_js_scraper/spiders/quotes.py
@@ -3,9 +3,20 @@ from quotes_js_scraper.items import QuoteItem
 
 
 class QuotesSpider(scrapy.Spider):
-    name = 'quotes'
-    allowed_domains = ['quotes.toscrape.com']
-    start_urls = ['http://quotes.toscrape.com/']
+    name = "quotes"
+    allowed_domains = ["quotes.toscrape.com"]
+
+    def start_requests(self):
+        url = "https://quotes.toscrape.com/js/"
+        yield scrapy.Request(url, meta={"playwright": True})
 
     def parse(self, response):
-        pass
+        for quote in response.css("div.quote"):
+            quote_item = QuoteItem()
+            quote_item["text"] = quote.css("span.text::text").get()
+            quote_item["author"] = quote.css("small.author::text").get()
+            quote_item["tags"] = quote.css("div.tags a.tag::text").getall()
+            self.logger.info(f"Quote: {quote_item}")
+            yield quote_item  # Corrected line
+
+        self.log("Saved all quotes")
