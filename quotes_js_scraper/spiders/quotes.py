ee36752dd0e135bf0e43560f666d98aa0a565534
Amine Daouma <amine.daouma.engie@gmail.com>
2024-09-22 19:18:58 +0200

    scraping multiple pages using Playwright on pagination

--- quotes_js_scraper/spiders/quotes.py
+++ quotes_js_scraper/spiders/quotes.py
@@ -19,16 +19,33 @@ class QuotesSpider(scrapy.Spider):
             errback=self.errback,
         )
 
-    def parse(self, response):
+    async def parse(self, response):
+        page = response.meta["playwright_page"]
+        await page.close()
+
         for quote in response.css("div.quote"):
             quote_item = QuoteItem()
             quote_item["text"] = quote.css("span.text::text").get()
             quote_item["author"] = quote.css("small.author::text").get()
             quote_item["tags"] = quote.css("div.tags a.tag::text").getall()
             self.logger.info(f"Quote: {quote_item}")
-            yield quote_item  # Corrected line
+            yield quote_item
 
         self.log("Saved all quotes")
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page is not None:
+            next_page_url = "https://quotes.toscrape.com" + next_page
+            yield scrapy.Request(
+                next_page_url,
+                meta=dict(
+                    playwright=True,
+                    playwright_include_page=True,
+                    playwright_page_methods=[
+                        PageMethod("wait_for_selector", "div.quote")
+                    ],
+                ),
+                errback=self.errback,
+            )
 
     async def errback(self, failure):
         self.logger.error(f"Failed to load page: {failure}")
